950141ea154900aef295600b7b2586bafa2493f7
lukaszdutka <lukaszdutka2@gmail.com>
2021-06-06 12:18:59 +0200

    create Button, Field and UserCreate components;

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import {Component} from "react";
+import UserCreate from "./UserCreate";
 
 class App extends Component {
     state = {language: 'english'};
@@ -15,7 +16,7 @@ class App extends Component {
                     <i className="flag us" onClick={() => this.onLanguageChange('english')}/>
                     <i className="flag nl" onClick={() => this.onLanguageChange('dutch')}/>
                 </div>
-                {this.state.language}
+                <UserCreate/>
             </div>
         );
     }
---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import {Component} from "react";
+
+
+class Button extends Component {
+    render() {
+        return <button className="ui primary">Submit</button>
+    }
+}
+
+export default Button;
